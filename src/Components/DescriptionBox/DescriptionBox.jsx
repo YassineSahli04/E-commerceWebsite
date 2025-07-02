93de823cccb71dfe759d3fe5bac8f2b67e8d7aa2
Yassine SAHLI <ysahl091@uottawa.ca>
2025-07-02 10:31:49 -0400

    added a review section

--- src/Components/DescriptionBox/DescriptionBox.jsx
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -1,59 +1,54 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
 import './DescriptionBox.css';
 import star_icon from "../Assets/star_icon.png";
 import star_dull_icon from "../Assets/star_dull_icon.png";
 
 const DescriptionBox = (props) => {
-  const { t } = useTranslation();
   const { product } = props;
 
   return (
     <div className='descriptionbox' role="region" aria-labelledby="reviews-heading">
       <div className="descriptionbox-navigator">
-        <div className="descriptionbox-nav-box">{t('reviews')}</div>
+        <div className="descriptionbox-nav-box">{'reviews'}</div>
       </div> 
       
       <div className="descriptionbox-description">
-        <h3 id="reviews-heading">{t('reviews_on')} {t(`product_${product.id}_description`)}</h3>
+        <h3 id="reviews-heading">Reviews On:  {product.description}</h3>
         
         <div className="review" role="article">
-          <p>{t('review_1')}:</p>
-          <div className="productdisplay-right-stars" role="img" aria-label={`${t('rating')} 5 ${t('stars')}`}>
-            <img src={star_icon} alt={t('filled_star')} />
-            <img src={star_icon} alt={t('filled_star')} />
-            <img src={star_icon} alt={t('filled_star')} />
-            <img src={star_icon} alt={t('filled_star')} />
-            <img src={star_icon} alt={t('filled_star')} />
+          <p>Jon Wolker</p>
+          <div className="review-stars" role="img" aria-label={`${'rating'} 5 ${'stars'}`}>
+            <img src={star_icon} alt={'filled_star'} />
+            <img src={star_icon} alt={'filled_star'} />
+            <img src={star_icon} alt={'filled_star'} />
+            <img src={star_icon} alt={'filled_star'} />
+            <img src={star_icon} alt={'filled_star'} />
           </div>
-          <p>{t('review_1_text')}</p>
-          <p><strong>{t('rating')}:</strong> 5/5</p>
+          <p>What a nice place to live. I hope someone will be able to afford it.</p>
         </div>
         
         <div className="review" role="article">
-          <p>{t('review_2')}:</p>
-          <div className="productdisplay-right-stars" role="img" aria-label={`${t('rating')} 4 ${t('stars')}`}>
-            <img src={star_icon} alt={t('filled_star')} />
-            <img src={star_icon} alt={t('filled_star')} />
-            <img src={star_icon} alt={t('filled_star')} />
-            <img src={star_icon} alt={t('filled_star')} />
-            <img src={star_dull_icon} alt={t('empty_star')} />
+          <p>Cathy Ross</p>
+          <div className="review-stars" role="img" aria-label={`${'rating'} 4 ${'stars'}`}>
+            <img src={star_icon} alt={'filled_star'} />
+            <img src={star_icon} alt={'filled_star'} />
+            <img src={star_icon} alt={'filled_star'} />
+            <img src={star_icon} alt={'filled_star'} />
+            <img src={star_dull_icon} alt={'empty_star'} />
           </div>
-          <p>{t('review_2_text')}</p>
-          <p><strong>{t('rating')}:</strong> 4/5</p>
+          <p>I would live here for the rest of my life.</p>
         </div>
         
         <div className="review" role="article">
-          <p>{t('review_3')}:</p>
-          <div className="productdisplay-right-stars" role="img" aria-label={`${t('rating')} 4 ${t('stars')}`}>
-            <img src={star_icon} alt={t('filled_star')} />
-            <img src={star_icon} alt={t('filled_star')} />
-            <img src={star_icon} alt={t('filled_star')} />
-            <img src={star_icon} alt={t('filled_star')} />
-            <img src={star_dull_icon} alt={t('empty_star')} />
+          <p>Emry Wild</p>
+          <div className="review-stars" role="img" aria-label={`${'rating'} 4 ${'stars'}`}>
+            <img src={star_icon} alt={'filled_star'} />
+            <img src={star_icon} alt={'filled_star'} />
+            <img src={star_icon} alt={'filled_star'} />
+            <img src={star_icon} alt={'filled_star'} />
+            <img src={star_dull_icon} alt={'empty_star'} />
           </div>
-          <p>{t('review_3_text')}</p>
-          <p><strong>{t('rating')}:</strong> 4/5</p>
+          <p>I really like this property. I would highly recommand.</p>
         </div>
       </div>
     </div>
